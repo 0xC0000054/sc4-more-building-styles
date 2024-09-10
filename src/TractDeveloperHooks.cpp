a9e22f83cbd6331242b09aa8d2032109f99661ea
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-09-09 19:04:09 -0600

    [WIP] Add a hook to cISC4TractDeveloper_IsBuildingCompatible
    It currently crashes after returning to SC4's code when the building
    doesn't support styles. No idea why that is.

--- src/TractDeveloperHooks.cpp
+++ src/TractDeveloperHooks.cpp
@@ -10,11 +10,19 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
+
 #include "TractDeveloperHooks.h"
+#include "cGZPersistResourceKey.h"
+#include "cIGZPersistResourceManager.h"
 #include "cIGZString.h"
+#include "cIGZVariant.h"
+#include "cISCProperty.h"
+#include "cISCPropertyHolder.h"
+#include "cISC4BuildingDevelopmentSimulator.h"
 #include "cISC4BuildingOccupant.h"
 #include "cISC4LotConfiguration.h"
-#include "cISC4TractDeveloper.h"
+#include "cRZAutoRefCount.h"
+#include "GZServPtrs.h"
 #include "Logger.h"
 #include "Patcher.h"
 #include "SC4String.h"
@@ -41,39 +49,39 @@ template<typename T> struct SC4Vector
 
 struct cSC4TractDeveloper
 {
-	void* vtable;                        // 0x0
-	void* messageTarget2;                // 0x4
-	void* gzSerialzable;                 // 0x8
-	int initialized;                     // 0xc
-	int refCount;                        // 0x10
-	void* pMS2;                          // 0x14
-	void* pCity;                         // 0x18
-	void* pBuildingDevelopmentSim;       // 0x1c
-	void* pLandValueSim;                 // 0x20
-	void* pLotConfigurationManager;      // 0x24
-	void* pLotDeveloper;                 // 0x28
-	void* pLotManager;                   // 0x2c
-	void* pPlumbingSim;                  // 0x30
-	void* pPowerSim;                     // 0x34
-	void* pTrafficSim;                   // 0x38
-	void* pZoneManager;                  // 0x3c
+	void* vtable;												// 0x0
+	void* messageTarget2;										// 0x4
+	void* gzSerialzable;										// 0x8
+	int initialized;											// 0xc
+	int refCount;												// 0x10
+	void* pMS2;													// 0x14
+	void* pCity;												// 0x18
+	cISC4BuildingDevelopmentSimulator* pBuildingDevelopmentSim; // 0x1c
+	void* pLandValueSim;										// 0x20
+	void* pLotConfigurationManager;								// 0x24
+	void* pLotDeveloper;										// 0x28
+	void* pLotManager;											// 0x2c
+	void* pPlumbingSim;											// 0x30
+	void* pPowerSim;											// 0x34
+	void* pTrafficSim;											// 0x38
+	void* pZoneManager;											// 0x3c
 	// begin tunable values
-	float overbuild;                     // 0x40
-	float reoccupyThresholds[2];         // 0x44
-	float redevelopSizeThreshold;        // 0x4c
-	float demolitionCosts[2];            // 0x50
-	float newLotCosts[2];                // 0x58
-	float occupancyVariation;            // 0x60
-	float minVacancyRange[2];            // 0x64
-	int kickOutLowerWealth;              // 0x6c
-	SC4Vector<uint32_t> availableStyles; // 0x70
-	int yearsBetweenStyles;              // 0x7c
+	float overbuild;											// 0x40
+	float reoccupyThresholds[2];								// 0x44
+	float redevelopSizeThreshold;								// 0x4c
+	float demolitionCosts[2];									// 0x50
+	float newLotCosts[2];										// 0x58
+	float occupancyVariation;									// 0x60
+	float minVacancyRange[2];									// 0x64
+	int kickOutLowerWealth;										// 0x6c
+	SC4Vector<uint32_t> availableStyles;						// 0x70
+	int yearsBetweenStyles;										// 0x7c
 	// end tunable values
-	uint8_t unknown1[0x98];              // 0x80 - 0x117
-	SC4Vector<uint32_t> activeStyles;    // 0x118
-	uint32_t currentStyleIndex;          // 0x124
-	uint32_t yearsPassed;                // 0x128
-	uint32_t notUsingAllStylesAtOnce;    // 0x12c
+	uint8_t unknown1[0x98];										// 0x80 - 0x117
+	SC4Vector<uint32_t> activeStyles;							// 0x118
+	uint32_t currentStyleIndex;									// 0x124
+	uint32_t yearsPassed;										// 0x128
+	uint32_t notUsingAllStylesAtOnce;							// 0x12c
 };
 
 static_assert(offsetof(cSC4TractDeveloper, initialized) == 0xc);
@@ -113,6 +121,23 @@ static bool LotConfigurationHasStyle(const cSC4LotConfiguration* pLotConfigurati
 		}
 
 		++pOccupantGroup;
+	}
+
+	return false;
+}
+
+
+static bool HasOccupantGroupValue(
+	const uint32_t* const pOccupantGroupData,
+	uint32_t occupantGroupCount,
+	uint32_t value)
+{
+	for (uint32_t i = 0; i < occupantGroupCount; i++)
+	{
+		if (pOccupantGroupData[i] == value)
+		{
+			return true;
+		}
 	}
 
 	return false;
@@ -249,6 +274,198 @@ static void NAKED_FUN IsLotConfigurationSuitable_BuildingStyleSelectionHook()
 	_asm ret
 }
 
+static void BuildingPurposeTypeDoesNotSupportStyles(
+	const cSC4TractDeveloper* pThis,
+	uint32_t buildingType,
+	cISC4BuildingOccupant::PurposeType purposeType)
+{
+	const char* name = "Unknown";
+
+	switch (purposeType)
+	{
+	case cISC4BuildingOccupant::PurposeType::None:
+		name = "None";
+		break;
+	case cISC4BuildingOccupant::PurposeType::Residence:
+		name = "Residence";
+		break;
+	case cISC4BuildingOccupant::PurposeType::Services:
+		name = "Services";
+		break;
+	case cISC4BuildingOccupant::PurposeType::Office:
+		name = "Office";
+		break;
+	case cISC4BuildingOccupant::PurposeType::Tourism:
+		name = "Tourism";
+		break;
+	case cISC4BuildingOccupant::PurposeType::Agriculture:
+		name = "Agriculture";
+		break;
+	case cISC4BuildingOccupant::PurposeType::Processing:
+		name = "Processing";
+		break;
+	case cISC4BuildingOccupant::PurposeType::Manufacturing:
+		name = "Manufacturing";
+		break;
+	case cISC4BuildingOccupant::PurposeType::HighTech:
+		name = "HighTech";
+		break;
+	case cISC4BuildingOccupant::PurposeType::Other:
+		name = "Other";
+		break;
+	}
+
+	Logger::GetInstance().WriteLineFormatted(
+		LogLevel::Info,
+		"0x%08x (%s): Purpose type %u (%s) does not support building styles.",
+		buildingType,
+		pThis->pBuildingDevelopmentSim->GetExemplarName(buildingType)->ToChar(),
+		static_cast<uint32_t>(purposeType),
+		name);
+}
+
+static void BuildingSupportsStyle(
+	const cSC4TractDeveloper* pThis,
+	uint32_t buildingType,
+	uint32_t style)
+{
+	Logger::GetInstance().WriteLineFormatted(
+		LogLevel::Info,
+		"0x%08x (%s) supports building style 0x%x.",
+		buildingType,
+		pThis->pBuildingDevelopmentSim->GetExemplarName(buildingType)->ToChar(),
+		style);
+}
+
+static void BuildingDoesNotSupportStyle(
+	const cSC4TractDeveloper* pThis,
+	uint32_t buildingType)
+{
+	Logger::GetInstance().WriteLineFormatted(
+		LogLevel::Info,
+		"0x%08x (%s) doesn't have any supported building styles.",
+		buildingType,
+		pThis->pBuildingDevelopmentSim->GetExemplarName(buildingType)->ToChar());
+}
+
+static bool __stdcall BuildingSupportsStyle(const cSC4TractDeveloper* pThis, uint32_t buildingType)
+{
+	bool result = false;
+
+	cGZPersistResourceKey key;
+
+	if (pThis->pBuildingDevelopmentSim->GetBuildingKeyFromType(buildingType, key))
+	{
+		cIGZPersistResourceManagerPtr pRM;
+
+		if (pRM)
+		{
+			cRZAutoRefCount<cISCPropertyHolder> pPropertyHolder;
+
+			if (pRM->GetResource(key, GZIID_cISCPropertyHolder, pPropertyHolder.AsPPVoid(), 0, nullptr))
+			{
+				constexpr uint32_t kOccupantGroupsProperty = 0xAA1DD396;
+
+				const cISCProperty* pProperty = pPropertyHolder->GetProperty(kOccupantGroupsProperty);
+
+				if (pProperty)
+				{
+					const cIGZVariant* pVariant = pProperty->GetPropertyValue();
+
+					if (pVariant)
+					{
+						const uint32_t* const pOccupantGroupData = pVariant->RefUint32();
+						const uint32_t count = pVariant->GetCount();
+
+						if (pThis->notUsingAllStylesAtOnce == 0)
+						{
+							// Use all styles at once.
+							const uint32_t* pStyle = pThis->activeStyles.mpBegin;
+
+							while (pStyle != pThis->activeStyles.mpEnd)
+							{
+								if (HasOccupantGroupValue(pOccupantGroupData, count, *pStyle))
+								{
+#ifdef _DEBUG
+									BuildingSupportsStyle(pThis, buildingType, *pStyle);
+#endif // _DEBUG
+									result = true;
+									break;
+								}
+								++pStyle;
+							}
+
+#ifdef _DEBUG
+							if (!result)
+							{
+								BuildingDoesNotSupportStyle(pThis, buildingType);
+							}
+#endif // _DEBUG
+						}
+						else
+						{
+							uint32_t activeStyle = pThis->activeStyles[pThis->currentStyleIndex];
+
+							result = HasOccupantGroupValue(pOccupantGroupData, count, activeStyle);
+#ifdef _DEBUG
+							if (result)
+							{
+								BuildingSupportsStyle(pThis, buildingType, activeStyle);
+							}
+							else
+							{
+								BuildingDoesNotSupportStyle(pThis, buildingType);
+							}
+#endif // _DEBUG
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return result;
+}
+
+static uintptr_t IsBuildingCompatible_CompatableStyleFound_Continue;
+static uintptr_t IsBuildingCompatible_NoCompatableStyle_Continue;
+
+static void NAKED_FUN IsBuildingCompatible_BuildingStyleSelectionHook()
+{
+	static const cSC4TractDeveloper* pThis;
+	static cISC4BuildingOccupant::PurposeType buildingPurpose;
+	static uint32_t buildingType;
+	static const uint32_t* pStyle;
+
+	_asm pushad
+	_asm mov pThis, esi
+	_asm mov eax, dword ptr[edi]
+	_asm mov buildingPurpose, eax
+	_asm mov eax, dword ptr[esp + 0x38]
+	_asm mov buildingType, eax
+
+	if (pThis->activeStyles.empty()
+		|| buildingPurpose < cISC4BuildingOccupant::PurposeType::Residence
+		|| buildingPurpose > cISC4BuildingOccupant::PurposeType::Office)
+	{
+		BuildingPurposeTypeDoesNotSupportStyles(pThis, buildingType, buildingPurpose);
+		_asm popad
+		_asm push IsBuildingCompatible_CompatableStyleFound_Continue
+		_asm ret
+	}
+
+	if (BuildingSupportsStyle(pThis, buildingType))
+	{
+		_asm popad
+		_asm push IsBuildingCompatible_CompatableStyleFound_Continue
+		_asm ret
+	}
+
+	_asm popad
+	_asm push IsBuildingCompatible_NoCompatableStyle_Continue
+	_asm ret
+}
+
 void TractDeveloperHooks::Install()
 {
 	Logger& logger = Logger::GetInstance();
@@ -256,6 +473,9 @@ void TractDeveloperHooks::Install()
 	uintptr_t IsLotConfigurationSuitable_BuildingStyleSelectionHook_Inject = 0;
 	IsLotConfigurationSuitable_CompatableStyleFound_Continue = 0;
 	IsLotConfigurationSuitable_NoCompatableStyle_Continue = 0;
+	uintptr_t IsBuildingCompatible_BuildingStyleSelectionHook_Inject = 0;
+	IsBuildingCompatible_CompatableStyleFound_Continue = 0;
+	IsBuildingCompatible_NoCompatableStyle_Continue = 0;
 
 	const uint16_t gameVersion = SC4VersionDetection::GetInstance().GetGameVersion();
 	bool setCallbacks = false;
@@ -266,6 +486,9 @@ void TractDeveloperHooks::Install()
 		IsLotConfigurationSuitable_BuildingStyleSelectionHook_Inject = 0x704ca2;
 		IsLotConfigurationSuitable_CompatableStyleFound_Continue = 0x704d22;
 		IsLotConfigurationSuitable_NoCompatableStyle_Continue = 0x704cda;
+		IsBuildingCompatible_BuildingStyleSelectionHook_Inject = 0x704e78;
+		IsBuildingCompatible_CompatableStyleFound_Continue = 0x704ef4;
+		IsBuildingCompatible_NoCompatableStyle_Continue = 0x704ed3;
 		setCallbacks = true;
 		break;
 	}
@@ -277,6 +500,9 @@ void TractDeveloperHooks::Install()
 			Patcher::InstallJump(
 				IsLotConfigurationSuitable_BuildingStyleSelectionHook_Inject,
 				reinterpret_cast<uintptr_t>(&IsLotConfigurationSuitable_BuildingStyleSelectionHook));
+			Patcher::InstallJump(
+				IsBuildingCompatible_BuildingStyleSelectionHook_Inject,
+				reinterpret_cast<uintptr_t>(&IsBuildingCompatible_BuildingStyleSelectionHook));
 
 			logger.WriteLine(LogLevel::Info, "Installed the building style algorithm patch.");
 		}
