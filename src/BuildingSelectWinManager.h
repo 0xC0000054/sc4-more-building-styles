944c3b50d77a2daaf5a4bc9ab485273cf4f1298b
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-01-24 04:35:02 -0700

    Bump copyright year

--- src/BuildingSelectWinManager.h
+++ src/BuildingSelectWinManager.h
@@ -3,7 +3,7 @@
 // This file is part of sc4-more-building-styles, a DLL Plugin for
 // SimCity 4 that adds support for more building styles.
 //
-// Copyright (c) 2024 Nicholas Hayes
+// Copyright (c) 2024, 2025 Nicholas Hayes
 //
 // This file is licensed under terms of the MIT License.
 // See LICENSE.txt for more information.
