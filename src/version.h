39bbcda44a8d668c5606fb683ccc0668a7592484
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-05-06 22:41:39 -0600

    Release version 3.3.2

--- src/version.h
+++ src/version.h
@@ -21,6 +21,6 @@
 
 #pragma once
 
-#define PLUGIN_VERSION_STR	    "3.3.1"
-#define RESOURCE_VERSION         3,3,1,0
-#define RESOURCE_VERSION_STR    "3.3.1.0"
+#define PLUGIN_VERSION_STR	    "3.3.2"
+#define RESOURCE_VERSION         3,3,2,0
+#define RESOURCE_VERSION_STR    "3.3.2.0"
