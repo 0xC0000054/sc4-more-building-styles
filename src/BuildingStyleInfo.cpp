0xC0000054/sc4-more-building-styles

commit f9e8d198064ec9fc183c69697f3be4f7c7241e03

Use a fall back name for style ids that are not in the UI
This prevents the DLL form returning an empty string in that case,
which could be confusing for style names that are shown to the user.

src/BuildingStyleInfo.cpp
@@ -37,6 +37,32 @@
 
 namespace
 {
+	void AppendBuildingStyleName(
+		const BuildingStyleCollection& availableBuildingStyles,
+		uint32_t styleID,
+		cIGZString& destination)
+	{
+		auto item = availableBuildingStyles.find_style(styleID);
+
+		if (item != availableBuildingStyles.end())
+		{
+			destination.Append(item->styleName);
+		}
+		else
+		{
+			// The specified style is not present in the UI, display a fall back string.
+
+			char buffer[128]{};
+
+			int length = std::snprintf(buffer, sizeof(buffer), "Unknown (0x%X)", styleID);
+
+			if (length > 0)
+			{
+				destination.Append(buffer, static_cast<uint32_t>(length));
+			}
+		}
+	}
+
 	void GetStyleNamesFromVariant(
 		const cIGZVariant& variant,
 		const BuildingStyleCollection& availableBuildingStyles,
@@ -56,20 +82,15 @@ namespace
 
 			for (size_t i = 0; i < repCount; i++)
 			{
-				auto item = availableBuildingStyles.find_style(pData[i]);
+				AppendBuildingStyleName(availableBuildingStyles, pData[i], destination);
 
-				if (item != availableBuildingStyles.end())
+				if (firstStyle)
 				{
-					if (firstStyle)
-					{
-						firstStyle = false;
-					}
-					else
-					{
-						destination.Append(separator);
-					}
-
-					destination.Append(item->styleName);
+					firstStyle = false;
+				}
+				else
+				{
+					destination.Append(separator);
 				}
 			}
 		}
@@ -77,12 +98,7 @@ namespace
 		{
 			const uint32_t targetStyleID = reinterpret_cast<uint32_t>(pData);
 
-			auto item = availableBuildingStyles.find_style(targetStyleID);
-
-			if (item != availableBuildingStyles.end())
-			{
-				destination.Append(item->styleName);
-			}
+			AppendBuildingStyleName(availableBuildingStyles, targetStyleID, destination);
 		}
 	}
 }
@@ -175,17 +191,10 @@ bool BuildingStyleInfo::GetBuildingStyleName(uint32_t style, cIGZString& name) c
 {
 	const BuildingStyleCollection& availableBuildingStyles = buildingWinManager.GetAvailableBuildingStyles();
 
-	bool result = false;
+	name.Erase(0, UINT_MAX);
+	AppendBuildingStyleName(availableBuildingStyles, style, name);
 
-	auto item = availableBuildingStyles.find_style(style);
-
-	if (item != availableBuildingStyles.end())
-	{
-		name = item->styleName;
-		result = true;
-	}
-
-	return result;
+	return name.Strlen() > 0;
 }
 
 bool BuildingStyleInfo::GetBuildingStyleNames(cISC4Occupant* pBuildingOccupant, cIGZString& destination) const

commit ab5f8d0beb329ab57c61225c585a84efb5545e43

Fix getting style names from the occupant groups
The fallback style name code in f9e8d198064ec9fc183c69697f3be4f7c7241e03
inadvertently caused the occupant group style code to see every
occupant group as a building style.
To fix this, the fallback style name code will only be used for the
building styles property.

This commit also includes a change that trims the trailing separator
from the output string, which saves callers from having to do it.

src/BuildingStyleInfo.cpp
@@ -37,7 +37,8 @@
 
 namespace
 {
-	void AppendBuildingStyleName(
+	template <bool useFallbackStyleName>
+	bool AppendBuildingStyleName(
 		const BuildingStyleCollection& availableBuildingStyles,
 		uint32_t styleID,
 		cIGZString& destination)
@@ -47,8 +48,9 @@ namespace
 		if (item != availableBuildingStyles.end())
 		{
 			destination.Append(item->styleName);
+			return true;
 		}
-		else
+		else if constexpr (useFallbackStyleName)
 		{
 			// The specified style is not present in the UI, display a fall back string.
 
@@ -59,10 +61,14 @@ namespace
 			if (length > 0)
 			{
 				destination.Append(buffer, static_cast<uint32_t>(length));
+				return true;
 			}
 		}
+
+		return false;
 	}
 
+	template <bool useFallbackStyleName>
 	void GetStyleNamesFromVariant(
 		const cIGZVariant& variant,
 		const BuildingStyleCollection& availableBuildingStyles,
@@ -74,7 +80,7 @@ namespace
 
 		if (repCount > 1)
 		{
-			bool firstStyle = true;
+			bool trimFinalSeparator = false;
 
 			// The styles are printed in the order that they are listed in the exemplar property.
 			// Previous versions of the DLL did this as a side effect of the implementation, and
@@ -82,23 +88,26 @@ namespace
 
 			for (size_t i = 0; i < repCount; i++)
 			{
-				AppendBuildingStyleName(availableBuildingStyles, pData[i], destination);
-
-				if (firstStyle)
-				{
-					firstStyle = false;
-				}
-				else
+				if (AppendBuildingStyleName<useFallbackStyleName>(availableBuildingStyles, pData[i], destination))
 				{
 					destination.Append(separator);
+					trimFinalSeparator = true;
 				}
+			}
+
+			if (trimFinalSeparator)
+			{
+				// Remove the trailing separator.
+
+				const uint32_t separatorLength = separator.Strlen();
+				destination.Erase(destination.Strlen() - separatorLength, separatorLength);
 			}
 		}
 		else
 		{
 			const uint32_t targetStyleID = repCount == 0 ? reinterpret_cast<uint32_t>(pData) : pData[0];
 
-			AppendBuildingStyleName(availableBuildingStyles, targetStyleID, destination);
+			AppendBuildingStyleName<useFallbackStyleName>(availableBuildingStyles, targetStyleID, destination);
 		}
 	}
 }
@@ -192,7 +201,7 @@ bool BuildingStyleInfo::GetBuildingStyleName(uint32_t style, cIGZString& name) c
 	const BuildingStyleCollection& availableBuildingStyles = buildingWinManager.GetAvailableBuildingStyles();
 
 	name.Erase(0, UINT_MAX);
-	AppendBuildingStyleName(availableBuildingStyles, style, name);
+	AppendBuildingStyleName<false>(availableBuildingStyles, style, name);
 
 	return name.Strlen() > 0;
 }
@@ -238,7 +247,7 @@ bool BuildingStyleInfo::GetBuildingStyleNamesEx(
 
 					if (pVariant)
 					{
-						GetStyleNamesFromVariant(
+						GetStyleNamesFromVariant<true>(
 							*pVariant,
 							availableBuildingStyles,
 							destination,
@@ -276,7 +285,7 @@ bool BuildingStyleInfo::GetBuildingStyleNamesEx(
 
 							if (pVariant)
 							{
-								GetStyleNamesFromVariant(
+								GetStyleNamesFromVariant<false>(
 									*pVariant,
 									availableBuildingStyles,
 									destination,